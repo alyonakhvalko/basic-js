4b758f505ef294f4a7b335b8dc82aa9e44e8fc46
Alyona Khvalko <alyonakazimirchick@gmail.com>
2022-05-03 01:13:15 +0300

    feat: add solution

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -12,13 +12,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 
-//удаляет наименьшее число в массиве, но не возвращает наибольшее из набора
 function deleteDigit(n) {
-  let arr = n.toString().split('').map(i => +i);
+
+let digits = String(n).split("");
+  let max = 0;
+  for (let i = 0; i < String(n).length; i++) {
+    let numbers = String(n).split("");
+    digits.splice(i, 1);
+    if (Number(digits.join("")) > max) {
+      max = Number(digits.join(""));
+      digits = numbers;
+    }
+      digits = numbers;
+  }
+    return max;
+
+
+
+
+//удаляет наименьшее число в массиве, но не возвращает наибольшее из набора
+  /*let arr = n.toString().split('').map(i => +i);
   arr = arr.slice(); //копировать arr
   arr.splice( arr.indexOf(Math.min.apply(null, arr)),1)
-  return +arr.join('');
+  return +arr.join('');*/
 }
+
+
 
 module.exports = {
   deleteDigit
