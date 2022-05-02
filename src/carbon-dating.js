257a9016dddc2f8ba814a30a1422c555415f8a99
Alyona Khvalko <alyonakazimirchick@gmail.com>
2022-05-02 11:40:20 +0300

    js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (
+    typeof sampleActivity != "string" ||
+    +(sampleActivity) <= 0 ||
+    +(sampleActivity) >= 15 || isNaN(+(sampleActivity))
+  ) {
+    return false;
+  }
+  let years = Math.log(MODERN_ACTIVITY / Number(sampleActivity)) / (0.693 / HALF_LIFE_PERIOD);
+  return Math.ceil(years);
 }
 
 module.exports = {
--- src/common-character-count.js
+++ src/common-character-count.js
@@ -12,7 +12,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 function getCommonCharacterCount(s1, s2) {
-  
+  let arr1 = s1.split('');
+  let arr2 = s2.split('');
+  let result = 0;
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        result ++;
+        arr2[j] = '';
+        break;
+      }
+    }
+  }
+  return result
 }
 
 module.exports = {
